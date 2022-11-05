7edf428c972bf54d7c1e0f394b7a59d67d03f99e
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-05 16:29:25 +0900

    express 요청 응답해보기

--- 2022풀스택부트캠프/backend/express_intro/firstApp/index.js
+++ 2022풀스택부트캠프/backend/express_intro/firstApp/index.js
@@ -6,15 +6,23 @@ const app = express();
 
 // use 메서드는 요청이 들어오면 콜백
 // 브라우저에서 보이진 않지만 새로고침하면 터미널에서 보인다.
-app.use(() => {
+app.use((req, res) => {
   console.log("유즈 사용하기");
+  // request 요청
+  //console.dir(req);
+
+  // http 응답을 보냄
+  //res.send("<h1>안녕하세요</h1>");
+
+  // javascript로 보내기
+  res.send({ color: "red" }); // JSON으로 변환되어 나타난다.
 });
 
 //express 실행
-app.listen(3000, (e) => {
-  console.log("LISTENING ON PROT 3000");
-});
-
-// app.listen(8080, (e) => {
-//   console.log("LISTENING ON PROT 8080");
+// app.listen(3000, (e) => {
+//   console.log("LISTENING ON PROT 3000");
 // });
+
+app.listen(8080, (e) => {
+  console.log("LISTENING ON PROT 8080");
+});
