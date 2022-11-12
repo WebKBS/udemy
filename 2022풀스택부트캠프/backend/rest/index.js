fd4e007d523db1e2a37e1f7405e551d44931a00c
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-12 19:48:06 +0900

    ejs를 이용해 가상 데이터 연결

--- 2022풀스택부트캠프/backend/rest/index.js
+++ 2022풀스택부트캠프/backend/rest/index.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // form urlencoded로 받기
 app.use(express.urlencoded({ extended: true }));
 
 // json 으로 받기
 app.use(express.json());
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+const comments = [
+  {
+    username: "Todd",
+    comment: "lol that is so funny",
+  },
+  {
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    username: "onlysaywoof",
+    comment: "woof woof woof",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
 
 app.get("/tacos", (req, res) => {
   res.send("Get /tacos response");
