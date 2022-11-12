cfb83af130bd9ebd23e2c4ea0186afcb1857708c
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-12 18:42:13 +0900

    rest, urlencoded, json으로 post 받아보기

--- 2022풀스택부트캠프/backend/rest/index.js
+++ 2022풀스택부트캠프/backend/rest/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+// form urlencoded로 받기
+app.use(express.urlencoded({ extended: true }));
+
+// json 으로 받기
+app.use(express.json());
+
 app.get("/tacos", (req, res) => {
   res.send("Get /tacos response");
 });
 
 app.post("/tacos", (req, res) => {
-  res.send("Post /tacos response");
+  const { meat, qty } = req.body;
+  res.send(`Ok, ${qty}, ${meat}`);
 });
 
 app.listen(3000, () => {
