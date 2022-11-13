21a031efbbadc0f3b18f1fc8cc37da366b099c62
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-13 20:25:25 +0900

    CRUD 중 Update form으로 사용하기

--- 2022풀스택부트캠프/backend/rest/index.js
+++ 2022풀스택부트캠프/backend/rest/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid");
 
 // form urlencoded로 받기
@@ -8,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // json 으로 받기
 app.use(express.json());
+
+app.use(methodOverride("_method"));
 
 app.set("views", path.join(__dirname, "views"));
 
@@ -60,6 +63,12 @@ app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
   const comment = comments.find((c) => c.id === id);
   res.render("comments/show", { comment });
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
 });
 
 app.patch("/comments/:id", (req, res) => {
