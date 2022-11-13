0dd09cfd7fe4da3f320dbbdf3cd8445366a92137
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-13 18:54:18 +0900

    uuid 사용해서 id값 지정하기

--- 2022풀스택부트캠프/backend/rest/index.js
+++ 2022풀스택부트캠프/backend/rest/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 
 // form urlencoded로 받기
 app.use(express.urlencoded({ extended: true }));
@@ -14,22 +15,22 @@ app.set("view engine", "ejs");
 
 const comments = [
   {
-    id: 1,
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny",
   },
   {
-    id: 2,
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    id: 3,
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
-    id: 4,
+    id: uuid(),
     username: "onlysaywoof",
     comment: "woof woof woof",
   },
@@ -50,14 +51,14 @@ app.get("/tacos", (req, res) => {
 app.post("/comments", (req, res) => {
   //console.log(req.body);
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   //res.send("IT Work"); // 이 상태로 전송했을때 새로고침을 하면 다시 post가 재 전송되서 중복이된다. redirect를 사용하자
   res.redirect("/comments"); // 데이터를 전송하고 지정한 path로 이동한다. 302
 });
 
 app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
-  const comment = comments.find((c) => c.id === parseInt(id));
+  const comment = comments.find((c) => c.id === id);
   res.render("comments/show", { comment });
 });
 
