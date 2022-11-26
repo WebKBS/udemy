15ec8d3f401dd8e82b0b9e99058714c2af919edf
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-26 18:47:28 +0900

    몽구스 model 사용해보기

--- 2022풀스택부트캠프/backend/MongooseBasics/index.js
+++ 2022풀스택부트캠프/backend/MongooseBasics/index.js
@@ -1,3 +1,5 @@
+// 영화앱을 가정
+
 const mongoose = require("mongoose"); //27017 기본 포트
 mongoose
   .connect("mongodb://localhost:27017/movieApp")
@@ -7,3 +9,18 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+const amadeus = new Movie({
+  title: "Amadeus",
+  year: 1986,
+  score: 9.2,
+  rating: "R",
+});
