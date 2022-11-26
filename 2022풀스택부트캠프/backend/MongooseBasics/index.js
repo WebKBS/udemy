41782e26e1fb586bafa2d91c562719c0f3632322
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-26 19:26:48 +0900

    몽구스에서 데이터 찾기

--- 2022풀스택부트캠프/backend/MongooseBasics/index.js
+++ 2022풀스택부트캠프/backend/MongooseBasics/index.js
@@ -25,13 +25,13 @@ const Movie = mongoose.model("Movie", movieSchema);
 //   rating: "R",
 // });
 
-Movie.insertMany([
-  { title: "Amelie", year: 2001, score: 8.3, rating: "R" },
-  { title: "Alien", year: 1979, score: 8.1, rating: "R" },
-  { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
-  { title: "Stand By me", year: 1986, score: 8.6, rating: "R" },
-  { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" },
-]).then((data) => {
-  console.log("It Worked");
-  console.log(data);
-});
+// Movie.insertMany([
+//   { title: "Amelie", year: 2001, score: 8.3, rating: "R" },
+//   { title: "Alien", year: 1979, score: 8.1, rating: "R" },
+//   { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
+//   { title: "Stand By me", year: 1986, score: 8.6, rating: "R" },
+//   { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" },
+// ]).then((data) => {
+//   console.log("It Worked");
+//   console.log(data);
+// });
