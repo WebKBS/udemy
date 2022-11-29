52bc40fa01609209ec3a20025376e71804d0552c
BeomSu-Kang <kbs21c1987@gmail.com>
2022-11-29 22:56:53 +0900

    product 만들어기

--- 2022풀스택부트캠프/backend/MongooseExpress/index.js
+++ 2022풀스택부트캠프/backend/MongooseExpress/index.js
@@ -18,11 +18,23 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   //console.log(products);
   res.render("products/index", { products }); // 랜더링을 위한 두번째 인수
+});
+
+app.get("/products/new", async (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  //res.send("making your product");
+  res.redirect(`/products/${newProduct._id}`); // 리다이랙트 요청한 페이지로 바로이동
 });
 
 app.get("/products/:id", async (req, res) => {
