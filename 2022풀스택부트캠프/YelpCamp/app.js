1de8216b576acb821c47a8a4c2d0351af2b917aa
BeomSu-Kang <kbs21c1987@gmail.com>
2022-12-03 20:55:41 +0900

    campground new, 생성 만들기

--- 2022풀스택부트캠프/YelpCamp/app.js
+++ 2022풀스택부트캠프/YelpCamp/app.js
@@ -20,12 +20,25 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true })); // url 인코드
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+
+// :id 보다 아래 있으면 안된다. 순서가 먼저 있어야 한다.
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
