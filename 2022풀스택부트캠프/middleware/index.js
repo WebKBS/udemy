366c166b9ad9883a2acfa27d22640aba629fdde3
BeomSu-Kang <kbs21c1987@gmail.com>
2022-12-08 23:56:26 +0900

    미들웨어 더알아보기 시간지정 및 POSTMAN 으로 요청 확인하기.

--- 2022풀스택부트캠프/middleware/index.js
+++ 2022풀스택부트캠프/middleware/index.js
@@ -6,45 +6,56 @@ const morgan = require("morgan");
 
 //morgan("tiny");
 
-app.use(morgan("dev")); // 요청 응답을 받고 종료한 시간 및 요청상태 코드를 콘솔에 표시해준다.
+//app.use(morgan("dev")); // 요청 응답을 받고 종료한 시간 및 요청상태 코드를 콘솔에 표시해준다.
+//app.use(morgan("common"));
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+  //req.method = "GET"; // 들어오는 요청을 모두다 GET으로 바꾼다. 굳이 요청을 재 정의 할 필요 없다.
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
+  next();
+}); // Postman으로 확인가능
 
 // app.use((req, res) => {
 //   res.send("HI JACK My App.Use"); // 아무 주소나 쳐도 요청된다..
 // });
 
-app.use((req, res, next) => {
-  console.log("MiddleWare Used!!! First");
-  next(); // 미들웨어 사용  **** 다음 미들웨어를 사용하려면 next()를 반드시 써줘야한다. next()를 사용하지않으면 다음은 실행되지 않음
-  // console은 나오지만, 페이지 요청은 불가능하다.
+// app.use((req, res, next) => {
+//   console.log("MiddleWare Used!!! First");
+//   next(); // 미들웨어 사용  **** 다음 미들웨어를 사용하려면 next()를 반드시 써줘야한다. next()를 사용하지않으면 다음은 실행되지 않음
+//   // console은 나오지만, 페이지 요청은 불가능하다.
 
-  console.log("MiddleWare - After Calling"); // next() 다음에 온 요청은 가장 마지막에 나타난다.
-});
+//   console.log("MiddleWare - After Calling"); // next() 다음에 온 요청은 가장 마지막에 나타난다.
+// });
 
-app.use((req, res, next) => {
-  console.log("MiddleWare Used!!! Second");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("MiddleWare Used!!! Second");
+//   next();
+// });
 
-app.use((req, res, next) => {
-  console.log("MiddleWare Used!!! Third");
-  return next();
-  console.log("return After"); // 실행되지 않는다.
-});
+// app.use((req, res, next) => {
+//   console.log("MiddleWare Used!!! Third");
+//   return next();
+//   console.log("return After"); // 실행되지 않는다.
+// });
 
-app.use((req, res, next) => {
-  console.log("MiddleWare Used!!! Four");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("MiddleWare Used!!! Four");
+//   next();
+// });
 
 // app.use(() => {
 //   console.log("HEy!!!!!"); // 요청, 응답이 종료되지 않는다.
 // });
 
 app.get("/", (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`);
   res.send("Home Page");
 });
 
 app.get("/dogs", (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`);
   res.send("Woof Woof!");
 });
 
