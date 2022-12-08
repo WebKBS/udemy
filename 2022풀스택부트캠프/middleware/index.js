e9be42b44b7dcced89d38ae83474b0631af2b9e9
BeomSu-Kang <kbs21c1987@gmail.com>
2022-12-08 23:35:05 +0900

    미들웨어 정의하기 next 파라미터 추가 후, next()실행

--- 2022풀스택부트캠프/middleware/index.js
+++ 2022풀스택부트캠프/middleware/index.js
@@ -1,3 +1,5 @@
+// https://expressjs.com/ko/guide/writing-middleware.html 미들웨어 작성 방법
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -5,6 +7,34 @@ const morgan = require("morgan");
 //morgan("tiny");
 
 app.use(morgan("dev")); // 요청 응답을 받고 종료한 시간 및 요청상태 코드를 콘솔에 표시해준다.
+
+// app.use((req, res) => {
+//   res.send("HI JACK My App.Use"); // 아무 주소나 쳐도 요청된다..
+// });
+
+app.use((req, res, next) => {
+  console.log("MiddleWare Used!!! First");
+  next(); // 미들웨어 사용  **** 다음 미들웨어를 사용하려면 next()를 반드시 써줘야한다. next()를 사용하지않으면 다음은 실행되지 않음
+  // console은 나오지만, 페이지 요청은 불가능하다.
+
+  console.log("MiddleWare - After Calling"); // next() 다음에 온 요청은 가장 마지막에 나타난다.
+});
+
+app.use((req, res, next) => {
+  console.log("MiddleWare Used!!! Second");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("MiddleWare Used!!! Third");
+  return next();
+  console.log("return After"); // 실행되지 않는다.
+});
+
+app.use((req, res, next) => {
+  console.log("MiddleWare Used!!! Four");
+  next();
+});
 
 // app.use(() => {
 //   console.log("HEy!!!!!"); // 요청, 응답이 종료되지 않는다.
