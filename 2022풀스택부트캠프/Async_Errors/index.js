9ca46be63417e905e625658f898eed4737e5caa9
BeomSu-Kang <kbs21c1987@gmail.com>
2022-12-17 23:49:11 +0900

    비동기 오류 처리하기

---
+++ 2022풀스택부트캠프/Async_Errors/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const AppError = require("./AppError");
+
+const mongoose = require("mongoose"); //27017 기본 포트
+
+const Product = require("./models/product");
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStand2")
+  .then(() => {
+    console.log("Mongo Connection Open");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("Mongo Error");
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+const categories = ["fruit", "vegetable", "dairy", "mushrooms", "fungi"];
+
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
+  //const products = await Product.find({});
+  //console.log(products);
+  //res.render("products/index", { products }); // 랜더링을 위한 두번째 인수
+});
+
+app.get("/products/new", async (req, res) => {
+  res.render("products/new", { categories });
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`); // 리다이랙트 요청한 페이지로 바로이동
+});
+
+app.get("/products/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    return next(new AppError("Product Not Found", 404)); // 비동기 에러는 next()를 사용해서 전달해야함. return을 사용해야 다음 코드를 실행하지 않는다
+  }
+  res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    return next(new AppError("Product Not Found", 404)); // 비동기 에러는 next()를 사용해서 전달해야함. return을 사용해야 다음 코드를 실행하지 않는다
+  }
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
+});
+
+app.listen(3000, () => {
+  console.log("App is litening 3000");
+});
