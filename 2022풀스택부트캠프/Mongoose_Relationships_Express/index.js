065c023b937363f3d28cc2d6fd68ac3c5d582cb5
BeomSu-Kang <kbs21c1987@gmail.com>
2022-12-31 23:17:16 +0900

    두개 프로젝트 연결하기

--- 2022풀스택부트캠프/Mongoose_Relationships_Express/index.js
+++ 2022풀스택부트캠프/Mongoose_Relationships_Express/index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-
-const mongoose = require("mongoose"); //27017 기본 포트
 
 const Product = require("./models/product");
 const Farm = require("./models/farm");
+const categories = ["fruit", "vegetable", "dairy"];
 
 mongoose
-  .connect("mongodb://localhost:27017/farmStandTake2")
-  .then(() => {
-    console.log("Mongo Connection Open");
+  .connect("mongodb://localhost:27017/farmStandTake2", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((error) => {
-    console.log(error);
-    console.log("Mongo Error");
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!");
+    console.log(err);
   });
 
 app.set("views", path.join(__dirname, "views"));
@@ -24,19 +27,24 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-//Farm Routes
+// FARM ROUTES
+
 app.get("/farms", async (req, res) => {
   const farms = await Farm.find({});
   res.render("farms/index", { farms });
 });
-
 app.get("/farms/new", (req, res) => {
   res.render("farms/new");
 });
-
 app.get("/farms/:id", async (req, res) => {
-  const farm = await Farm.findById(req.params.id);
+  const farm = await Farm.findById(req.params.id).populate("products");
   res.render("farms/show", { farm });
+});
+
+app.delete("/farms/:id", async (req, res) => {
+  const farm = await Farm.findByIdAndDelete(req.params.id);
+
+  res.redirect("/farms");
 });
 
 app.post("/farms", async (req, res) => {
@@ -45,8 +53,25 @@ app.post("/farms", async (req, res) => {
   res.redirect("/farms");
 });
 
-//products Route
-const categories = ["fruit", "vegetable", "dairy", "mushrooms", "fungi"];
+app.get("/farms/:id/products/new", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  res.render("products/new", { categories, farm });
+});
+
+app.post("/farms/:id/products", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const { name, price, category } = req.body;
+  const product = new Product({ name, price, category });
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${id}`);
+});
+
+// PRODUCT ROUTES
 
 app.get("/products", async (req, res) => {
   const { category } = req.query;
@@ -57,24 +82,21 @@ app.get("/products", async (req, res) => {
     const products = await Product.find({});
     res.render("products/index", { products, category: "All" });
   }
-  //const products = await Product.find({});
-  //console.log(products);
-  //res.render("products/index", { products }); // 랜더링을 위한 두번째 인수
 });
 
-app.get("/products/new", async (req, res) => {
+app.get("/products/new", (req, res) => {
   res.render("products/new", { categories });
 });
 
 app.post("/products", async (req, res) => {
   const newProduct = new Product(req.body);
   await newProduct.save();
-  res.redirect(`/products/${newProduct._id}`); // 리다이랙트 요청한 페이지로 바로이동
+  res.redirect(`/products/${newProduct._id}`);
 });
 
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate("farm", "name");
   res.render("products/show", { product });
 });
 
@@ -95,10 +117,10 @@ app.put("/products/:id", async (req, res) => {
 
 app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const deleteProduct = await Product.findByIdAndDelete(id);
+  const deletedProduct = await Product.findByIdAndDelete(id);
   res.redirect("/products");
 });
 
 app.listen(3000, () => {
-  console.log("App is litening 3000");
+  console.log("APP IS LISTENING ON PORT 3000!");
 });
