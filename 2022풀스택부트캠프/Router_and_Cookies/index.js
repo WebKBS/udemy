95176e5a57d91f0677704b406bbb79a73bbc7504
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-03 22:38:00 +0900

    라우터와 미들웨어 사용해보기

--- 2022풀스택부트캠프/Router_and_Cookies/index.js
+++ 2022풀스택부트캠프/Router_and_Cookies/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 const sheltersRoutes = require("./routes/shelters");
 const dogRoutes = require("./routes/dogs");
+const adminRoutes = require("./routes/admin");
+
+app.use((req, res, next) => {
+  if (req.query.isAdmin) {
+    next();
+
+    //http://localhost:3000/admin/topsecret?isAdmin=true 설정해야 이동함
+  }
+  res.send("Sorry Not Admin");
+});
 
 app.use("/breeders", sheltersRoutes);
 app.use("/dogs", dogRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(3000, () => {
   console.log("Port 3000!!!");
