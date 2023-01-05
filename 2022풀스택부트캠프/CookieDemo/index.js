2688f60d4d0efb7fbbb2bee0e795eb6b93bd7b9e
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-05 23:52:42 +0900

    쿠키 서명하기

--- 2022풀스택부트캠프/CookieDemo/index.js
+++ 2022풀스택부트캠프/CookieDemo/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 
-app.use(cookieParser());
+app.use(cookieParser("thisismysecret"));
 
 app.get("/greet", (req, res) => {
   const { name = "No-name" } = req.cookies;
@@ -13,6 +13,18 @@ app.get("/greet", (req, res) => {
 app.get("/setname", (req, res) => {
   res.cookie("name2", "stevie");
   res.send("Your Cookie");
+});
+
+app.get("/getcookies", (req, res) => {
+  res.cookie("fruit", "grape", { signed: true });
+  res.send("get Cookies!!");
+});
+
+app.get("/verifyfruit", (req, res) => {
+  // console.log(req.cookies);
+  // res.send(req.cookies);
+  console.log(req.signedCookies); //signedCookies 서명된 쿠키 호출
+  res.send(req.signedCookies);
 });
 
 app.listen(3000, () => {
