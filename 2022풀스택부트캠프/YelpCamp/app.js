663eae788e0d0187db13d39a496828ce4112f330
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-07 18:47:18 +0900

    campground session 추가하기.

--- 2022풀스택부트캠프/YelpCamp/app.js
+++ 2022풀스택부트캠프/YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 
 const ExpressError = require("./utils/expressError");
 const methodOverride = require("method-override");
@@ -30,8 +31,19 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); // url 인코드
 app.use(methodOverride("_method"));
-
 app.use(express.static(path.join(__dirname, "public"))); // 등록된 폴더 js파일을 불러올수 있음
+
+const sessionConfig = {
+  secret: "thisshouldbesecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
