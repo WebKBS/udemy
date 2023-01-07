f5ddba3fcc722b9198f8268740876b505827c5f0
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-08 01:21:40 +0900

    flash error 메세지 추가

--- 2022풀스택부트캠프/YelpCamp/routes/campgrounds.js
+++ 2022풀스택부트캠프/YelpCamp/routes/campgrounds.js
@@ -51,7 +51,10 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       "reviews"
     );
-
+    if (!campground) {
+      req.flash("error", "Cannot find that campground!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
   })
 );
@@ -60,6 +63,10 @@ router.get(
   "/:id/edit",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "Cannot find that campground!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
