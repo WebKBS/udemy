6c749a22ed6ef34e084d531ff9e9c5e078a36839
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-09 22:42:56 +0900

    bcrypt를 사용한 간단한 로그인 구현하기

--- 2022풀스택부트캠프/AuthDemo/index.js
+++ 2022풀스택부트캠프/AuthDemo/index.js
@@ -1,12 +1,58 @@
 const express = require("express");
 const app = express();
 const User = require("./models/user");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+mongoose
+  .connect("mongodb://localhost:27017/authDemo")
+  .then(() => {
+    console.log("Mongo Connect!!");
+  })
+  .catch((error) => {
+    console.log("Mongo Error!!");
+    console.log(error);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("This is homePage");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+
+  await user.save();
+
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (validPassword) {
+    res.send("Welcommmme!!");
+  } else {
+    res.send("Try again");
+  }
 });
 
 app.get("/secret", (req, res) => {
