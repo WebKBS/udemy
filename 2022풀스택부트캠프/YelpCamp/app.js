2d5443879e4f8a231b0bac2599d9a3654608662e
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-11 22:12:17 +0900

    fake로 간단하게 사용해보기

--- 2022풀스택부트캠프/YelpCamp/app.js
+++ 2022풀스택부트캠프/YelpCamp/app.js
@@ -6,8 +6,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/expressError");
 const methodOverride = require("method-override");
-
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -47,11 +48,22 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("Error");
   next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({ email: "kang@gmail.com", username: "Kang" });
+  const newUSer = await User.register(user, "chicken");
+  res.send(newUSer);
 });
 
 app.use("/campgrounds", campgrounds);
