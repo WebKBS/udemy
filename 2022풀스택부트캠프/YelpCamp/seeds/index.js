cff4b5184114ad9569da87a93a7f668f06361981
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-14 19:11:17 +0900

    author 추가하기

--- 2022풀스택부트캠프/YelpCamp/seeds/index.js
+++ 2022풀스택부트캠프/YelpCamp/seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "63c1595138f4be9e5d78fb3e", // 사용자 지정
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
