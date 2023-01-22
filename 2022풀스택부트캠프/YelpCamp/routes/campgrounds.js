e37d12526b05e4d4f6c7db8c40b6af98f903b274
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-22 18:22:37 +0900

    route 사용해서 중복 줄이고 리팩토링하기

--- 2022풀스택부트캠프/YelpCamp/routes/campgrounds.js
+++ 2022풀스택부트캠프/YelpCamp/routes/campgrounds.js
@@ -6,40 +6,34 @@ const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
 const Campground = require("../models/campground");
 
-router.get("/", isLoggedIn, catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 // :id 보다 아래 있으면 안된다. 순서가 먼저 있어야 한다.
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
