c074d4cf6bc97ff4293ed3052a004fcd88629904
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-22 21:22:39 +0900

    multer라이브러리을 이용한 파일업로드 셋팅하기

--- 2022풀스택부트캠프/YelpCamp/routes/campgrounds.js
+++ 2022풀스택부트캠프/YelpCamp/routes/campgrounds.js
@@ -3,17 +3,22 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const Campground = require("../models/campground");
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => { // single은 파일 하나일때, array는 파일 여러개일때
+    console.log(req.body, req.file);
+  });
 
 // :id 보다 아래 있으면 안된다. 순서가 먼저 있어야 한다.
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
