fa88adbc923e6fa1489e80cfcf7a70fe68e077fb
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-28 20:31:01 +0900

    map box로 위치 알아보기. geocode 만들기(맵등록시작)

--- 2022풀스택부트캠프/YelpCamp/controllers/campgrounds.js
+++ 2022풀스택부트캠프/YelpCamp/controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,19 +14,23 @@ module.exports.renderNewForm = async (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
-  // if (!req.body.campground)
-  //   throw new ExpressError("Invalid Campground Data", 404); // 따로 에러메세지 지정하기, 포스트맨에서 확인가능
-
-  const campground = new Campground(req.body.campground);
-  campground.images = req.files.map((f) => ({
-    url: f.path,
-    filename: f.filename,
-  }));
-  campground.author = req.user._id;
-  await campground.save();
-  console.log(campground.images);
-  req.flash("success", "Successfully made a new campground");
-  res.redirect(`/campgrounds/${campground._id}`);
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
+  res.send(geoData.body.features[0].geometry.coordinates);
+  // const campground = new Campground(req.body.campground);
+  // campground.images = req.files.map((f) => ({
+  //   url: f.path,
+  //   filename: f.filename,
+  // }));
+  // campground.author = req.user._id;
+  // await campground.save();
+  // console.log(campground.images);
+  // req.flash("success", "Successfully made a new campground");
+  // res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.showCampground = async (req, res) => {
